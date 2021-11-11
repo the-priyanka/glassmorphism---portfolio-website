d7c35e9d9a075f83f37283244f088949c7b375dd
Priyanka <original.manaria@gmail.com>
2021-11-11 17:18:17 +0530

    some chages

--- index.js
+++ index.js
@@ -20,37 +20,3 @@ tabsContainer.addEventListener("click", (e) => {
   }
 });
 
-// -----------------Portfolio Item Details-------------------
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("view-project-btn")) {
-    togglePortfolioPopup();
-    document.querySelector("portfolio-popup").scrollTo(0, 0);
-    portfolioItemDetails(e.target.parentElement);
-  }
-});
-
-function togglePortfolioPopup() {
-  portfolioPopup.classList.toggle("open");
-  document.body.classList.toggle("hide-scrolling");
-  main.classList.toggle("fade-out");
-}
-
-// hide popup when clicking outside of it
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("pp-inner")) {
-    togglePortfolioPopup();
-  }
-});
-
-ppClose.addEventListener("click", togglePortfolioPopup);
-
-function portfolioItemDetails(portfolio) {
-  document.querySelector(".pp-thumbnail img").src =
-    portfolio.querySelector(".portfolio-item-thumbnail img").src;
-
-  document.querySelector(".pp-header h3").innerHTML =
-    portfolio.querySelector(".portfolio-item-title").innerHTML;
-
-  document.querySelector(".pp-body").innerHTML =
-    portfolio.querySelector(".portfolio-item-details").innerHTML;
-}
