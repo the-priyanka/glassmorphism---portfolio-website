8e23b23c6f2649b243408522bdc0062019c1bc54
Priyanka <original.manaria@gmail.com>
2021-11-16 15:05:54 +0530

    contact section ended

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const tabsContainer = document.querySelector(".about-tabs");
 const aboutSection = document.querySelector(".about-section");
 const portfolioPopup = document.querySelector(".portfolio-popup");
 const ppClose = document.querySelector(".pp-close");
-const main = document.querySelector(".main"); 
+const main = document.querySelector(".main");
 
 tabsContainer.addEventListener("click", (e) => {
   if (
@@ -20,3 +20,37 @@ tabsContainer.addEventListener("click", (e) => {
   }
 });
 
+// -----------------Portfolio Item Details-------------------
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("view-project-btn")) {
+    togglePortfolioPopup();
+    document.querySelector(".portfolio-popup").scrollTo(0, 0);
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+
+function togglePortfolioPopup() {
+  portfolioPopup.classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+  main.classList.toggle("fade-out");
+}
+
+// hide popup when clicking outside of it
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("pp-inner")) {
+    togglePortfolioPopup();
+  }
+});
+
+ppClose.addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolio) {
+  document.querySelector(".pp-thumbnail img").src =
+    portfolio.querySelector(".portfolio-item-thumbnail img").src;
+
+  document.querySelector(".pp-header h3").innerHTML =
+    portfolio.querySelector(".portfolio-item-title").innerHTML;
+
+  document.querySelector(".pp-body").innerHTML =
+    portfolio.querySelector(".portfolio-item-details").innerHTML;
+}
