17cd7a54e354b098513a121504b2d6fe2df7c2b2
Priyanka <original.manaria@gmail.com>
2021-11-17 16:59:19 +0530

    create a header section

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
+// ------------------- Toggle Navbar ------------------------
+const navToggle = document.querySelector(".nav-toggle");
+
+const header = document.querySelector(".header");
+
+navToggle.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
+});
+function hideSection() {
+  document
+    .querySelector("section.active")
+    .classList.toggle("fade-out");
+}
+function toggleNavbar() {
+  header.classList.toggle("active");
+}
+
+// ------------------- Active Section ------------------------
+document.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("link-item") &&
+    e.target.hash !== ""
+  ) {
+    navToggle.classList.add("hide")
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add("hide-scrolling");
+    }
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove("hide-scrolling");
+      navToggle.classList.remove("hide")
+    }, 500);
+  }
+});
+
 // ------------------- About Tabs ------------------------
 const tabsContainer = document.querySelector(".about-tabs");
 const aboutSection = document.querySelector(".about-section");
